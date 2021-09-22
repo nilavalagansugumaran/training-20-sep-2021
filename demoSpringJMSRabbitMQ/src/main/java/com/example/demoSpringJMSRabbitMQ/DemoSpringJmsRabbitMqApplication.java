a66f3a092fdc55edeaf533a50d613130ca71f0b8
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2021-09-22 14:32:11 +0100

    RabbitsENDER

--- demoSpringJMSRabbitMQ/src/main/java/com/example/demoSpringJMSRabbitMQ/DemoSpringJmsRabbitMqApplication.java
+++ demoSpringJMSRabbitMQ/src/main/java/com/example/demoSpringJMSRabbitMQ/DemoSpringJmsRabbitMqApplication.java
@@ -2,12 +2,15 @@ package com.example.demoSpringJMSRabbitMQ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoSpringJmsRabbitMqApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringJmsRabbitMqApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(DemoSpringJmsRabbitMqApplication.class, args);
+		ctx.getBean(SenderComponent.class).sendUserData();
 	}
 
 }
--- demoSpringJMSRabbitMQ/src/main/java/com/example/demoSpringJMSRabbitMQ/SenderComponent.java
+++ demoSpringJMSRabbitMQ/src/main/java/com/example/demoSpringJMSRabbitMQ/SenderComponent.java
@@ -1,15 +1,24 @@
 package com.example.demoSpringJMSRabbitMQ;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class SenderComponent {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     public void sendUserData() {
         log.info("Sending user data starts...");
-
+        rabbitTemplate.convertAndSend("ex.nila.exchange",
+                "k.nila.key", User.builder().id(123).name("nila").build(), message -> {
+                    message.getMessageProperties().setHeader("myheader", "header value");
+                    return message;
+                });
         log.info("Sending user data finishes...");
     }
 }
